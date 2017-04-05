b47653ea9d2d7ab2f030759de94833e467689bb9
woutermertens <wouter.mertens@student.howest.be>
2017-04-05 22:36:29 +0200

    crashes because trainingdata is empty

--- NeuralNetworkTest/neural-net-tutorial.cpp
+++ NeuralNetworkTest/neural-net-tutorial.cpp
@@ -245,9 +245,6 @@ int main()
 {
 	//e.g., {3,2,1}
 	std::vector<unsigned> topology;
-	topology.push_back(3);
-	topology.push_back(2);
-	topology.push_back(1);
 	Net myNet(topology);
 
 	std::vector<double> input;
